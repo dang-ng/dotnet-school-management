2ec8e3976c29b0a289fcc88f3dcff11c14717834
derek089 <nguyen.hai.dang@icloud.com>
2022-12-27 11:04:38 +0700

    final commit

---
+++ SimpleQueries/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using DataAccess;
+using Models;
+
+namespace SimpleQueries
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new UniversityContext())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                var rs = context.Entry(context.Courses).Collection("Students").Query();
+            }
+            
+        }
+    }
+}
---
+++ UpdateSingle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using DataAccess;
+using Models;
+
+namespace UpdateSingle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
